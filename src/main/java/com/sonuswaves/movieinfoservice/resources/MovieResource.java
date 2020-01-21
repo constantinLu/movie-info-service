1e886d69625bf40e172715e7ee76239f84787b8b
gizet <lunguucatalin@gmail.com>
2020-01-21 14:46:22 +0200

    [MovieInfo] - Initialize the app

--- src/main/java/com/sonuswaves/movieinfoservice/resources/MovieResource.java
+++ src/main/java/com/sonuswaves/movieinfoservice/resources/MovieResource.java
@@ -14,7 +14,6 @@ public class MovieResource {
 
     @RequestMapping("/{movieId}")
     public Movie getMovieInfo(@PathVariable("movieId") String movieId) {
-
         return new Movie(movieId, "Test name");
     }
 
